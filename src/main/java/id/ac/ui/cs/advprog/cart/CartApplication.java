e7ef28fd692e655889de57393505b18d1b7f9e0d
shanahandnlf <shanahan.danualif@ui.ac.id>
2024-04-17 15:40:40 +0700

    first commit

---
+++ src/main/java/id/ac/ui/cs/advprog/cart/CartApplication.java
@@ -0,0 +1,13 @@
+package id.ac.ui.cs.advprog.cart;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CartApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CartApplication.class, args);
+    }
+
+}
---
+++ src/test/java/id/ac/ui/cs/advprog/cart/CartApplicationTests.java
@@ -0,0 +1,13 @@
+package id.ac.ui.cs.advprog.cart;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CartApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
