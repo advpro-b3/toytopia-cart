9fe6604c98c807cb2b22f1b5c0509b6697ec1601
shanahandnlf <shanahan.danualif@ui.ac.id>
2024-04-24 19:43:55 +0700

    try to make homepage

--- src/main/java/id/ac/ui/cs/advprog/cart/controller/ShoppingCartController.java
+++ src/main/java/id/ac/ui/cs/advprog/cart/controller/ShoppingCartController.java
@@ -18,51 +18,5 @@ public class ShoppingCartController {
         this.shoppingCartService = shoppingCartService;
     }
 
-    @GetMapping("/all")
-    public ResponseEntity<List<ShoppingCart>> getAll() {
-        List<ShoppingCart> all = shoppingCartService.findAll();
-        if (all != null) {
-            return ResponseEntity.ok(all);
-        } else {
-            return ResponseEntity.notFound().build();
-        }
-    }
 
-    @GetMapping("/cart-{userId}")
-    public ResponseEntity<ShoppingCart> getByUserId(@PathVariable(name = "userId") Long userId) {
-        ShoppingCart shoppingCart = shoppingCartService.findById(userId);
-        if (shoppingCart != null) {
-            return ResponseEntity.ok(shoppingCart);
-        } else {
-            return ResponseEntity.notFound().build();
-        }
-    }
-
-    @PostMapping("/create")
-    public ResponseEntity<ShoppingCart> create(@RequestBody ShoppingCart shoppingCart) {
-        ShoppingCart created = shoppingCartService.create(shoppingCart);
-        if (created != null) {
-            return ResponseEntity.ok(created);
-        } else {
-            return ResponseEntity.notFound().build();
-        }
-    }
-
-    @PutMapping("/edit")
-    public ResponseEntity<ShoppingCart> edit(@RequestBody ShoppingCart shoppingCart) {
-        ShoppingCart edited = shoppingCartService.edit(shoppingCart);
-        if (edited != null) {
-            return ResponseEntity.ok(edited);
-        } else {
-            return ResponseEntity.notFound().build();
-        }
-    }
-
-    @DeleteMapping("/delete/{userId}")
-    public ResponseEntity<ShoppingCart> delete(@PathVariable(name = "userId") Long userId) {
-        ShoppingCart shoppingCart = new ShoppingCart();
-        shoppingCart.setUserId(userId);
-        shoppingCartService.delete(shoppingCart);
-        return ResponseEntity.ok().build();
-    }
 }
