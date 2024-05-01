a29a15d3df85e34cccc5c3120a5903b7642da1f6
shanahandnlf <shanahan.danualif@ui.ac.id>
2024-05-01 09:29:41 +0700

    [GREEN] pass tests for shopping cart repository

--- src/main/java/id/ac/ui/cs/advprog/cart/model/ShoppingCart.java
+++ src/main/java/id/ac/ui/cs/advprog/cart/model/ShoppingCart.java
@@ -1,15 +1,23 @@
 package id.ac.ui.cs.advprog.cart.model;
 
+import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
 
 import java.util.HashMap;
 import java.util.Map;
 
+import com.fasterxml.jackson.annotation.JsonIdentityInfo;
+import com.fasterxml.jackson.annotation.ObjectIdGenerators;
+
 @Setter
 @Getter
+@Entity
 public class ShoppingCart {
+    @Id
     private Long userId;
+    @OneToMany(mappedBy = "shoppingCart", cascade = CascadeType.ALL, orphanRemoval = true)
+    @MapKey(name = "productId")
     private Map<String, CartItem> cartItemMap;
 
     public ShoppingCart() {
@@ -19,5 +27,24 @@ public class ShoppingCart {
     public void addItem(CartItem item) {
         cartItemMap.put(item.getProductId(), item);
     }
+
+    public void removeItem(String productId) {
+        cartItemMap.remove(productId);
+    }
+
+    public double calculateTotalPrice() {
+        return cartItemMap.values().stream()
+                .mapToDouble(item -> item.getPrice() * item.getQuantity())
+                .sum();
+    }
+
+    public Map<String, CartItem> getAllCartItem() {
+        return cartItemMap;
+    }
+
+    public CartItem addItemToCart(CartItem item) {
+        return cartItemMap.put(item.getProductId(), item);
+    }
+
 
 }
