1a6a8b5f902d87d492fef5fa8e4224fa19116da2
shanahandnlf <shanahan.danualif@ui.ac.id>
2024-05-15 17:22:16 +0700

    [FEAT] try to enable async and make threadpool task executor

--- src/main/java/id/ac/ui/cs/advprog/cart/CartApplication.java
+++ src/main/java/id/ac/ui/cs/advprog/cart/CartApplication.java
@@ -3,6 +3,10 @@ package id.ac.ui.cs.advprog.cart;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
+
 @SpringBootApplication
 public class CartApplication {
 
@@ -10,4 +14,14 @@ public class CartApplication {
         SpringApplication.run(CartApplication.class, args);
     }
 
+    @Bean
+    public ThreadPoolTaskExecutor taskExecutor() {
+        ThreadPoolTaskExecutor executor = new ThreadPoolTaskExecutor();
+        executor.setCorePoolSize(2);
+        executor.setMaxPoolSize(2);
+        executor.setQueueCapacity(500);
+        executor.setThreadNamePrefix("Cart-");
+        executor.initialize();
+        return executor;
+    }
 }
